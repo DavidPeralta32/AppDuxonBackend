64e21e1782299c1e69da19bb547f5329fe1ac54a
David Peralta <davi32peralta@gmail.com>
2023-04-10 19:08:13 -0600

    se implementa la funcion de alta de registro patronal

--- controllers/contabilidad/contabilidadController.js
+++ controllers/contabilidad/contabilidadController.js
@@ -35,3 +35,34 @@ exports.getRegistroPatronal = async (req, res, next) => {
         console.log(error)
     }
 }
+
+exports.altaRegistroPatronal = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    const nRegistroPatronal = req.body.nRegistroPatronal;
+    const dfechaAlta = req.body.dFechaAlta;
+    const nEstado = 1;
+    const sZonaCiudad = req.body.sZonaCiudad;
+    const nSalarioMinimoZPR = req.body.nSalarioMinimoZPR;
+    const nPrimaRiesgo = req.body.nPrimaRiesgo;
+    const nElementos = req.body.nElementos;
+
+
+    sql = "INSERT INTO registro_patronal (ID,nRegistroPatronal,dFechaAlta,nEstado,sZonaCiudad,nSalarioMinimoZRP,PrimaDeRiesgo, nElementos) VALUES(null,'"+nRegistroPatronal+"','"+dfechaAlta+"',"+nEstado+",'"+sZonaCiudad+"',"+nSalarioMinimoZPR+","+nPrimaRiesgo+","+nElementos+")";
+
+    try {
+        conn.conexion().query(sql, (error, results) => {
+            if (error) {
+                res.json(error);
+            } else { 
+                if (results.affectedRows > 0) {
+                    res.json(results);
+                }
+            }
+        });
+        //cerrar
+
+    } catch (error) {
+        console.log(error)
+    }
+}
--- routes/contabilidad.js
+++ routes/contabilidad.js
@@ -3,6 +3,7 @@ const router= express.Router()
 const contabilidadController= require('../controllers/contabilidad/contabilidadController');
 
 router.post('/getRegistroPatronal', contabilidadController.getRegistroPatronal);
+router.post('/altaRegistroPatronal', contabilidadController.altaRegistroPatronal);
 
 
 
