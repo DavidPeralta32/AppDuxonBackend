a962c6bb19ba7e65af299beb1a10bb2eda250499
David Peralta <davi32peralta@gmail.com>
2023-04-11 19:11:39 -0600

    rutas y funcionalidad para editar y ver registro patronal

--- controllers/contabilidad/contabilidadController.js
+++ controllers/contabilidad/contabilidadController.js
@@ -19,7 +19,27 @@ close = conexion.cerrarConexion
 exports.getRegistroPatronal = async (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
 
-    sql = `select ID, nRegistroPatronal, date_format(dFechaAlta,"%d-%m-%Y") as dFechaAlta, nEstado, sZonaCiudad,nSalarioMinimoZRP, PrimaDeRiesgo, nElementos from registro_patronal`
+    sql = `select ID as nID, nRegistroPatronal, date_format(dFechaAlta,"%d-%m-%Y") as dFechaAlta, nEstado, sZonaCiudad,nSalarioMinimoZRP, PrimaDeRiesgo, nElementos from registro_patronal WHERE nEstado = 1`;
+
+    try {
+        conn.conexion().query(sql, (error, results) => {
+            if (error) {
+                res.json(error);
+            } else {
+                res.json(results);
+            }
+
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.getRegistroPatronalBaja = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    sql = `select ID as nID, nRegistroPatronal, date_format(dFechaBaja,"%d-%m-%Y") as dFechaBaja, nEstado, sZonaCiudad,nSalarioMinimoZRP, PrimaDeRiesgo, nElementos, sMotivoBaja from registro_patronal WHERE nEstado = 2`;
 
     try {
         conn.conexion().query(sql, (error, results) => {
@@ -66,3 +86,121 @@ exports.altaRegistroPatronal = async (req, res, next) => {
         console.log(error)
     }
 }
+
+exports.bajaRegistroPatronal = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    const nRegistroPatronal = req.body.nRegistroPatronal;
+    const dFechaBaja = req.body.dFechaBaja;
+    const sMotivoBaja = req.body.sMotivoBaja;
+
+    sql = "UPDATE registro_patronal SET nEstado = 2, dFechaBaja = '"+dFechaBaja+"', sMotivoBaja = '"+sMotivoBaja+"' WHERE nRegistroPatronal = '"+nRegistroPatronal+"' ";
+
+    try {
+        conn.conexion().query(sql, (error, results) => {
+            if (error) {
+                res.json(error);
+            } else { 
+                if (results.affectedRows > 0) {
+                    res.json(results);
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.activarRegistroPatronal = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    const nRegistroPatronal = req.body.nRegistroPatronal;
+
+    sql = "UPDATE registro_patronal SET nEstado = 1 WHERE nRegistroPatronal = '"+nRegistroPatronal+"' ";
+
+    try {
+        conn.conexion().query(sql, (error, results) => {
+            if (error) {
+                res.json(error);
+            } else { 
+                if (results.affectedRows > 0) {
+                    res.json(results);
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.getRegistroPatronalxId = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    const nRegistroPatronal = req.body.nRegistroPatronal;
+
+    sql = "select ID as nID, nRegistroPatronal, date_format(dFechaAlta,'%d-%m-%Y') as dFechaAlta, nEstado, sZonaCiudad,nSalarioMinimoZRP, PrimaDeRiesgo, nElementos from registro_patronal WHERE nRegistroPatronal  = '"+nRegistroPatronal+"'";
+
+    try {
+        conn.conexion().query(sql, (error, results) => {
+            if (error) {
+                res.json(error);
+            } else {
+                res.json(results);
+            }
+
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+exports.editarRegistroPatronal = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    const nID = req.body.nID;
+    const nRegistroPatronal = req.body.nRegistroPatronal;
+    const sZonaCiudad =  req.body.sZonaCiudad;
+    const nSalarioMinimoZRP =  req.body.nSalarioMinimoZRP;
+    const PrimaDeRiesgo = req.body.PrimaDeRiesgo;
+    const nElementos = req.body.nElementos;
+
+    sql = "UPDATE registro_patronal SET nRegistroPatronal = '"+nRegistroPatronal+"', sZonaCiudad = '"+sZonaCiudad+"', nSalarioMinimoZRP = "+nSalarioMinimoZRP+", PrimaDeRiesgo = "+PrimaDeRiesgo+", nElementos = "+nElementos+" WHERE ID = '"+nID+"' ";
+
+    try {
+        conn.conexion().query(sql, (error, results) => {
+            if (error) {
+                res.json(error);
+            } else { 
+                if (results.affectedRows > 0) {
+                    res.json(results);
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.validarNRegistroPatronal = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    const nRegistroPatronal = req.body.nRegistroPatronal;
+
+    sql = "select nRegistroPatronal from registro_patronal WHERE nRegistroPatronal = '"+nRegistroPatronal+"'";
+
+    try {
+        conn.conexion().query(sql, (error, results) => {
+            if (error) {
+                res.json(error);
+            } else {
+                res.json(results);
+            }
+
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
--- routes/contabilidad.js
+++ routes/contabilidad.js
@@ -3,7 +3,13 @@ const router= express.Router()
 const contabilidadController= require('../controllers/contabilidad/contabilidadController');
 
 router.post('/getRegistroPatronal', contabilidadController.getRegistroPatronal);
+router.post('/getRegistroPatronalBaja', contabilidadController.getRegistroPatronalBaja);
 router.post('/altaRegistroPatronal', contabilidadController.altaRegistroPatronal);
+router.post('/bajaRegistroPatronal', contabilidadController.bajaRegistroPatronal);
+router.post('/activarRegistroPatronal', contabilidadController.activarRegistroPatronal);
+router.post('/getRegistroPatronalxId', contabilidadController.getRegistroPatronalxId);
+router.post('/editarRegistroPatronal', contabilidadController.editarRegistroPatronal);
+router.post('/validarNRegistroPatronal', contabilidadController.validarNRegistroPatronal);
 
 
 
